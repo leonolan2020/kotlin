cd0aa3d4e2ebf062858a1b42dff47e5efadebc8f
Andrei Klunnyi <andrei.klunnyi@jetbrains.com>
2020-12-18 15:47:00 +0100

    KTIJ-717 [Java side inspection]: // review: precise highlighting

--- idea/src/org/jetbrains/kotlin/idea/inspections/KotlinSealedInheritorsInJavaInspection.kt
+++ idea/src/org/jetbrains/kotlin/idea/inspections/KotlinSealedInheritorsInJavaInspection.kt
@@ -6,8 +6,10 @@
 package org.jetbrains.kotlin.idea.inspections
 
 import com.intellij.codeInspection.LocalInspectionTool
+import com.intellij.codeInspection.ProblemHighlightType
 import com.intellij.codeInspection.ProblemsHolder
 import com.intellij.psi.*
+import com.intellij.psi.impl.source.PsiClassReferenceType
 import org.jetbrains.kotlin.asJava.classes.KtUltraLightClass
 import org.jetbrains.kotlin.descriptors.isSealed
 import org.jetbrains.kotlin.idea.KotlinBundle
@@ -15,14 +17,20 @@ import org.jetbrains.kotlin.idea.KotlinBundle
 
 class KotlinSealedInheritorsInJavaInspection : LocalInspectionTool() {
     companion object {
-        private fun PsiClass.extendsKotlinSealed(): Boolean {
-            return when (this) {
-                is PsiAnonymousClass -> baseClassType.isKotlinSealed()
-                else -> extendsList?.includesKotlinSealed() == true || implementsList?.includesKotlinSealed() == true
-            }
+        private fun PsiClass.listSealedParentReferences(): List<PsiReference> {
+            if (this is PsiAnonymousClass && baseClassType.isKotlinSealed())
+                return listOf(baseClassReference)
+
+            val sealedBaseClasses = extendsList?.listSealedMembers()
+            val sealedBaseInterfaces = implementsList?.listSealedMembers()
+
+            return sealedBaseClasses.orEmpty() + sealedBaseInterfaces.orEmpty()
         }
 
-        private fun PsiReferenceList.includesKotlinSealed(): Boolean = referencedTypes.any { it.isKotlinSealed() }
+        private fun PsiReferenceList.listSealedMembers(): List<PsiReference>? = referencedTypes
+            ?.filter { it.isKotlinSealed() }
+            ?.mapNotNull { it as? PsiClassReferenceType }
+            ?.map { it.reference }
 
         private fun PsiClassType.isKotlinSealed(): Boolean = resolve()?.isKotlinSealed() == true
 
@@ -35,8 +43,12 @@ class KotlinSealedInheritorsInJavaInspection : LocalInspectionTool() {
     override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor {
         return object : JavaElementVisitor() {
             override fun visitClass(aClass: PsiClass?) {
-                if (aClass?.extendsKotlinSealed() == true)
-                    holder.registerProblem(aClass, KotlinBundle.message("inheritance.of.kotlin.sealed", aClass.abstractionTypeName))
+                aClass?.listSealedParentReferences()?.forEach {
+                    holder.registerProblem(
+                        it, KotlinBundle.message("inheritance.of.kotlin.sealed", aClass.abstractionTypeName),
+                        ProblemHighlightType.GENERIC_ERROR_OR_WARNING
+                    )
+                }
             }
 
             override fun visitAnonymousClass(aClass: PsiAnonymousClass?) = visitClass(aClass)
